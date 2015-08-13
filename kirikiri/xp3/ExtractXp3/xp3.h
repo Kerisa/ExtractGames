0894e581c3c5972e82fa861d26ad1ef8da65691b
Kerisa <segistration@gmail.com>
2015-08-13 21:45:51 +0800

    swansong

--- kirikiri/xp3/ExtractXp3/xp3.h
+++ kirikiri/xp3/ExtractXp3/xp3.h
@@ -39,6 +39,7 @@ int xp3_extract_file_save(HANDLE hFile, u8 *xp3_idx, int idx_len, u32 *file_num,
 typedef void (*_XOR_DECODE)(DWORD hash, u8 extend_key, u32 offset, PBYTE buf, DWORD len);
  void xor_decode				(DWORD hash, u8 extend_key, u32 offset, PBYTE buf, DWORD len);
  void xor_decode_prettycation	(DWORD hash, u8 extend_key, u32 offset, PBYTE buf, DWORD len);
+ void xor_decode_swansong	(DWORD hash, u8 extend_key, u32 offset, PBYTE buf, DWORD len);
 
 
 static const char unencry_game [] = "Unencrypted";		// 数据没有加密的游戏
@@ -49,6 +50,7 @@ static const struct _SIMPLE_XOR		// 数据xor过的游戏
 	u8    extend_key;	// 除去hash外的额外的key
 	u32   offset;		// 从数据的offset字节开始解码
 }
-simple_xor_game [] = {"kuranokunchi",	xor_decode,					0xCD, 0x0,
+simple_xor_game [] = { "kuranokunchi",	xor_decode,					0xCD, 0x0,
 					   "amakoi",		xor_decode,					 0x0, 0x0,
-					   "prettycation",	xor_decode_prettycation,	 0x0, 0x5};
+					   "prettycation",	xor_decode_prettycation,	 0x0, 0x5,
+					   "swansong",		xor_decode_swansong,		 0x0, 0x0,};
