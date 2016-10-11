895377724c5a96cc2d432554e1aa272d471f4ff6
Kerisa <segistration@gmail.com>
2016-10-12 00:57:58 +0800

    .

--- AdvHD/Merge2010/main.cpp
+++ AdvHD/Merge2010/main.cpp
@@ -13,28 +13,28 @@ Merge merge;
 
 int wmain(int argc, wchar_t **argv)
 {
-	//
-	// argv[1] - TxtFilename
-	// argv[2] - groupNum
-	// argv[3] - group0MaxId
-	// argv[4] - group1MaxId
-	// ...
-	//
-	if (argc < 5)		// 至少有两个分组
-	{
-		printf("Usage:\n    Merge.exe <TXTFilename> "
+    //
+    // argv[1] - TxtFilename
+    // argv[2] - groupNum
+    // argv[3] - group0MaxId
+    // argv[4] - group1MaxId
+    // ...
+    //
+    if (argc < 5)       // 至少有两个分组
+    {
+        printf("Usage:\n    Merge.exe <TXTFilename> "
             "<groupNum> <group0MaxId> <group1MaxId> [...]\n");
-		return 0;
-	}
+        return 0;
+    }
 
-	std::vector<int> g;
-	for (int i = 3; i < argc; ++i)
-		g.push_back(_wtoi(argv[i]));
+    std::vector<int> g;
+    for (int i = 3; i < argc; ++i)
+        g.push_back(_wtoi(argv[i]));
 
     merge.Initialize(argv[1], _wtoi(argv[2]), g);
-	merge.Process();
+    merge.Process();
     merge.Release();
 
-	//system("pause");
+    //system("pause");
     return 0;
 }
