63fd6d37061ba0186dc8f1c0be5a0d2ee7aeeb80
Kerisa <segistration@gmail.com>
2018-08-17 01:21:28 +0800

    移动目录

--- DDSystem/src/DDSystem/main.cpp
+++ DDSystem/src/DDSystem/main.cpp
@@ -9,11 +9,11 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-	if (argc != 3)
-	{
-		cout << "usage: DDSystem.exe <package file> <output directory>.\n";
-		return 0;
-	}
+    if (argc != 3)
+    {
+        cout << "usage: DDSystem.exe <package file> <output directory>.\n";
+        return 0;
+    }
 
     DDSystem dds;
     if (!dds.Open(argv[1]))
@@ -33,13 +33,13 @@ int main(int argc, char** argv)
         dir += '\\';
 
     
-	if (!dds.ExtractResource(dir))
-	{
-		cout << "some resource save failed.\n";
-		return 0;
-	}
+    if (!dds.ExtractResource(dir))
+    {
+        cout << "some resource save failed.\n";
+        return 0;
+    }
 
-	dds.Close();
-	
+    dds.Close();
+    
     return 0;
 }
